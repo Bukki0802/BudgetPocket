b725398124731a8da78ecce60d95df5f81eeb317
Bukki0802 <104125628+Bukki0802@users.noreply.github.com>
2026-06-25 15:16:36 +0900

    Configure Firebase web app

--- WebApp/firebase-config.js
+++ WebApp/firebase-config.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBuL82WiY-7QrzKoCBseZiq4t-JBiVJf_E",
+  authDomain: "moneyflow-e1462.firebaseapp.com",
+  projectId: "moneyflow-e1462",
+  storageBucket: "moneyflow-e1462.firebasestorage.app",
+  messagingSenderId: "673360393364",
+  appId: "1:673360393364:web:3ab09b7bd343493a1d7081",
+  measurementId: "G-041T40P60D"
 };
